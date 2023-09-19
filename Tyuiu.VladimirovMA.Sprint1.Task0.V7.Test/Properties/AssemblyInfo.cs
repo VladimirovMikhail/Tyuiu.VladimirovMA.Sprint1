9529db55b2411f58cc00571dc9e20b393d40cf95
Tyuiu.VladimirovMA@outlook.com <Tyuiu.VladimirovMA@outlook.com>
2023-09-20 00:41:11 +0500

    Add project files.

---
+++ Tyuiu.VladimirovMA.Sprint1.Task0.V7.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.VladimirovMA.Sprint1.Task0.V7.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.VladimirovMA.Sprint1.Task0.V7.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("65e615f7-d087-42a4-8967-6325c87a9c3c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.VladimirovMA.Sprint1.Task0.V7/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.VladimirovMA.Sprint1.Task0.V7.Lib;
+
+namespace Tyuiu.VladimirovMA.Sprint1.Task0.V7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*Спринт #1                                                                *");
+            Console.WriteLine("*Тема: Базовые навыки работы в С#                                         *");
+            Console.WriteLine("*Задание: #0                                                              *");
+            Console.WriteLine("*Вариант: #7                                                              *");
+            Console.WriteLine("*Выполнил Владимиров Михаил Александрович | АсоиУб-23-2                   *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*УСЛОВИЕ:                                                                 *");
+            Console.WriteLine("*Написать программу, которая вычисляет 18/6*2-(4-3)                       *");
+            Console.WriteLine("*и печатает результат на экране                                           *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*ИСХОДНЫЕ ДАННЫЕ:                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*18 / 6 * 2 - (4 - 3)                                                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*РЕЗУЛЬТАТ:                                                               *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(ds.Calculate());
+
+            Console.ReadLine();
+        }
+    }
+}
